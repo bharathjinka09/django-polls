fa5ce83bd962c935ec53e0ea93442be82c186634
bharath <bharathjinka09@gmail.com>
2019-09-05 13:29:33 +0530

    added

--- pollster/settings.py
+++ pollster/settings.py
@@ -50,7 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.storage.CompressedManifestStaticFilesStorage',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'pollster.urls'
